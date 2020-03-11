b641cb8bffddb85902717531e11b7aab46ee8bc9
Jun Kudo <jun-lab@junnoMacBook-Pro.local>
2020-03-11 18:27:51 +0900

    add create_details

--- features/base.py
+++ features/base.py
@@ -1,7 +1,6 @@
 from abc import ABCMeta, abstractmethod
 import argparse
 import inspect
-from logs.time_keeper import stop_watch
 import pandas as pd
 from pathlib import Path
 import pickle
@@ -15,23 +14,20 @@ class Feature(metaclass=ABCMeta):
     Inherit this class and, implement create_features method.
     """
     prefix = ''
-    suffix = ''
+    suffix = '' # ex) period, version, train, test
     dir = '.'
     
     def __init__(self):
         self.name = self.__class__.__name__
-        self.train = pd.DataFrame()
-        self.test = pd.DataFrame()
-        self.train_path = Path(self.dir) / f'{self.name}_train.pkl'
-        self.test_path = Path(self.dir) / f'{self.name}_test.pkl'
+        self.df = pd.DataFrame()
+        self.path = Path(self.dir) / f'{self.name}.pkl'
     
-    @stop_watch(self.name)
+    # @stop_watch(self.name)
     def run(self):
         self.create_features()
         prefix = self.prefix + '_' if self.prefix else ''
         suffix = '_' + self.suffix if self.suffix else ''
-        self.train.columns = prefix + self.train.columns + suffix
-        self.test.columns = prefix + self.test.columns + suffix
+        self.df.columns = prefix + self.df.columns + suffix
         return self
     
     @abstractmethod
@@ -39,27 +35,45 @@ class Feature(metaclass=ABCMeta):
         raise NotImplementedError
     
     def save(self):
-        self.train.to_pickle(str(self.train_path))
-        self.test.to_pickle(str(self.test_path))
+        self.df.to_pickle(str(self.path))
 
     def load(self):
-        self.train = pd.read_pickle(str(self.train_path))
-        self.test = pd.read_pickle(str(self.test_path))
+        self.df = pd.read_pickle(str(self.path))
 
+    def create_details(self,file_name,description):
+        file_path = dir + file_name
+        col_name = self.__class__.__name__
 
+        if not os.path.isfile(file_path):
+            with open(file_path,"w"):pass
+
+        with open(file_path,"r+") as f:
+            lines = f.readlines()
+            lines = [line.strip() for line in lines]
+
+            col = [line for line in lines if line.split(',')[0] == col_name ]
+            if len(col)!=0:
+                return
+
+            writer = csv.writer()
+            writer.writerow([col_name,description])
 """
 command line tool
 """
 def get_arguments():
     """
-    -f [--force] overwrite mode
+    -f [--force, --overwrite] overwrite mode
     """
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--force', '-f', action='store_true', help='Overwrite existing files')
+    parser = argparse.ArgumentParser(description='Process features.')
+    parser.add_argument('-f', '--force', '--overwrite',action='store_true', help='Overwrite existing files')
+
     return parser.parse_args()
 
 
 def get_features(namespace):
+    """
+    Featureを継承したクラスをインスタンス化して返すiterator
+    """
     for k, v in namespace.items():
         if inspect.isclass(v) and issubclass(v, Feature) and not inspect.isabstract(v):
             yield v()
@@ -68,10 +82,13 @@ def get_features(namespace):
 def generate_features(namespace, overwrite):
     """
     namespaceを渡してそこに含まれる特徴量が保存済みかどうか確認
-    存在しない場合は計算
+    [1] 存在しない場合 -> 計算
+    [2] 存在する場合 -> 処理をスキップ
+    [3] overwriteモード -> 計算
     """
     for f in get_features(namespace):
         if f.train_path.exists() and f.test_path.exists() and not overwrite:
             print(f.name, 'was skipped')
         else:
             f.run().save()
+
